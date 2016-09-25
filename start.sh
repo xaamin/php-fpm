7be2635b363ed2f5be1ad7b7724113cc0328a087
Benjamín Martínez Mateos <xaamin@outlook.com>
2016-09-25 11:47:18 -0500

    New sites strategy

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOGS="/shared/accounts/${ACCOUNT:=$(hostname)}/logs"
+LOGS="/shared/${ACCOUNT:=$(hostname)}/logs"
 PHPPID="/run/php"
 
 # Create logs directory
@@ -19,6 +19,6 @@ if [[ ! -d "$PHPPID" ]]; then
 	mkdir "$PHPPID"
 fi
 
-sed -i 's|error_log =.*|error_log = /shared/accounts/'${ACCOUNT:=$(hostname)}'/logs/php7.0-fpm.error.log|' /etc/php/7.0/fpm/php-fpm.conf
+sed -i 's|error_log =.*|error_log = /shared/'${ACCOUNT:=$(hostname)}'/logs/php7.0-fpm.error.log|' /etc/php/7.0/fpm/php-fpm.conf
 
 /usr/bin/supervisord -n
