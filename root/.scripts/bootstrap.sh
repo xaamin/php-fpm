45268ae4eebd65f38151cf96d7f1c802804b3612
Benjamín Martínez Mateos <xaamin@outlook.com>
2018-04-04 07:00:24 -0500

    Fixed missing entry for supervisor

--- root/.scripts/bootstrap.sh
+++ root/.scripts/bootstrap.sh
@@ -17,6 +17,10 @@ if [[ ! -d "$PHPPID" ]]; then
 	echo "Creating PHP FPM PID dir..."
 	echo ""
 	mkdir "$PHPPID"
+	echo "PHP FPM PID dir created"
+	echo ""
 fi
 
 sed -i 's|error_log =.*|error_log = /shared/'${ACCOUNT:=$(hostname)}'/logs/php7.1-fpm.error.log|' /etc/php/7.1/fpm/php-fpm.conf
+
+/usr/bin/supervisord -n
