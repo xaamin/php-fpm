5c5bae4eb5e6d19e5b49f524f8de3c08c498cf0c
Benjamín Martínez Mateos <xaamin@outlook.com>
2018-08-26 22:25:20 -0500

    Compliant with new nginx strategy

--- root/.scripts/bootstrap.sh
+++ root/.scripts/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-LOGS="/shared/web/${ACCOUNT:=$(hostname)}/logs"
+LOGS="${SHARED_VOLUME}/shared/server/log/${ACCOUNT:=$(hostname)}"
 PHPPID="/run/php"
 
-# Create logs directory
+# Create log directory
 if [[ ! -d "$LOGS" ]]; then
     echo "Creating log dir..."
 
@@ -18,23 +18,24 @@ if [[ ! -d "$PHPPID" ]]; then
     echo "Creating PHP FPM PID dir..."
 
     mkdir "$PHPPID"
-    echo "Created PHP FPM PID dir..."
 
-    echo ""
+    echo "Created PHP FPM PID dir..."
 fi
 
 /bin/bash /root/.scripts/fix-permissions.sh || true
 
-if [[ ! -f "/shared/web/${ACCOUNT:=$(hostname)}/permission-fixes.lock" ]]; then
+if [[ ! -f "${LOGS}/permission-fixes.lock" ]]; then
     /bin/bash /root/.scripts/apply-permissions.sh || true
 
-    touch "/shared/web/${ACCOUNT:=$(hostname)}/permission-fixes.lock"
+    touch "${LOGS}/permission-fixes.lock"
 
     echo "Created lock file to avoid apply permissions on every container start"
 else
     echo "Permissions fixes was done previously. Run the  apply-permissions.sh script after delete the permission-fixes.lock file"
+    echo "PHP FPM PID dir created"
+    echo ""
 fi
 
-sed -i 's|error_log =.*|error_log = /shared/web/'${ACCOUNT:=$(hostname)}'/logs/php7.0-fpm.error.log|' /etc/php/7.0/fpm/php-fpm.conf || true
+sed -i 's|error_log =.*|error_log = '${LOGS}'/php_error.log|' /etc/php/7.0/fpm/php-fpm.conf || true
 
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
